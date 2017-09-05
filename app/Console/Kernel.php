a44720517dc63c62e64abd3403d51517f4c75cc5
felipemlopes <felipemarcanthlopes@gmail.com>
2017-09-05 16:57:21 -0300

    commit

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -13,7 +13,9 @@ class Kernel extends ConsoleKernel
      * @var array
      */
     protected $commands = [
-        //
+        Commands\SetGreetingText::class,
+        Commands\SetGetStartedButton::class,
+        Commands\SetPersistentMenu::class,
     ];
 
     /**
--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,7 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::get("/trivia", "MainController@receive")->middleware("verify");
+Route::post("/trivia", "MainController@receive");
